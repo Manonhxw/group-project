c9fc7543cec518244a5974607987445699297034
lx <lx@163.com>
2020-08-05 17:32:05 +0800

    master update

--- tg-parent/tg-eureka/src/main/java/com/kgc/EurekaServerApp.java
+++ tg-parent/tg-eureka/src/main/java/com/kgc/EurekaServerApp.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApp {
     public static void main(String[] args) {
+        //启动eureka
         SpringApplication.run(EurekaServerApp.class,args);
     }
 }
